3e423059361b602f10b60b679da4d2b69cd91e18
Malcolm Anderson <malcolminyo@gmail.com>
2022-04-02 21:52:27 -0700

    Added inverse lerp and remap functions to QInterpolationTools

--- PyQtPlus/QtCorePlus/QInterpolationTools.py
+++ PyQtPlus/QtCorePlus/QInterpolationTools.py
@@ -4,3 +4,12 @@ class QInterpolationTools:
     @classmethod
     def lerp(cls, a: Union[float, int], b: Union[float, int], t: float) -> float:
         return a + (b - a) * t
+
+    @classmethod
+    def invLerp(cls, v: Union[float, int], a: Union[float, int], b: Union[float, int]) -> float:
+        return (v - a) / (b - a)
+
+    @classmethod
+    def remap(cls, inMin: Union[float, int], inMax: Union[float, int], outMin: Union[float, int], outMax: Union[float, int], v: Union[float, int]):
+        t = cls.invLerp(v, inMin, inMax)
+        return cls.lerp(outMin, outMax, t)
